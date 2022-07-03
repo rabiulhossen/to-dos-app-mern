6706dee4b24042b73c7a7c618ceee3d98375c393
rabiulhossen <rhsimon69@gmail.com>
2022-07-03 20:56:07 +0600

    completed pages done

---
+++ src/pages/CompletedTask.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+
+export default function CompletedTask() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/add")
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks(data);
+      });
+  }, []);
+
+  return (
+    <div>
+     <h2 className="my-3">Completed Task</h2>
+      <Table striped bordered hover variant="dark">
+       
+           
+         {
+          tasks.map((task,index)=>task.completed === true && <tbody key={index}>
+<tr><td>{task.completed}</td> </tr>
+<tr><td>{task.inputValue}</td> </tr>
+          </tbody>)
+          
+         }
+        
+        
+      </Table>
+    </div>
+  );
+}
